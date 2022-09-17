6ade655f12b5f03ef0b6282d36cae7e607e6dc15
Marduk_James <mjamesmcneill@gmail.com>
2022-09-17 17:40:13 -0400

    Added more to sniffer.py and README.md

--- packet_sniffer/README.md
+++ packet_sniffer/README.md
@@ -1,6 +1,9 @@
+Started 9-16-22
+Marduk_James
+
 Making a packet sniffer for my home network.
 One of the first steps I'm taking to sercuring my setup.
 
 I will list any resources I used here and try to comment throughout the way:
-    https://www.youtube.com/watch?v=WGJC5vT5YJo
+    https://www.youtube.com/user/thenewboston
     https://www.w3schools.com/python/python_reference.asp
--- packet_sniffer/sniffer.py
+++ packet_sniffer/sniffer.py
@@ -3,8 +3,30 @@ import struct
 import textwrap
 
 
-# Unpack ethernet frame
-# MAC(Media Access Control) address is a unique 12 character alphanumeric attribute (00:B0:D0:63:C2:26)
+def main():
+    conn + socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+
+
+
+
+
+
+
+
+# Unpack ethernet frame ( (SYNC 8byte):(RECIEVER 6byte):(SENDER 6byte):(TYPE 2byte):(PAYLOAD 46byte-1500byte):(CRC 4byte) )
+# MAC(Media Access Control) address - unique 12 character alphanumeric attribute (00:B0:D0:63:C2:26)
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+# Return a properly formatted MAC address.
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_addr).upper()
+    return mac_addr
+
+
+main()
